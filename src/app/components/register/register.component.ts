234f9289259d122445e9ec4b999fd42d2821db6a
Michell2022 <sistemas.angel04@gmail.com>
2023-02-16 00:28:48 -0500

    diseño registro

--- src/app/components/register/register.component.ts
+++ src/app/components/register/register.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component } from '@angular/core';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.css']
 })
-export class RegisterComponent {
+export class RegisterComponent implements AfterViewInit{
+
+  ngAfterViewInit(): void {
+    const iconEye: HTMLElement | null = document.querySelector('.icon-eye');
+    const passwordInput: HTMLInputElement | null = iconEye?.nextElementSibling as HTMLInputElement;
+  
+    iconEye?.addEventListener('click', () => {
+      if (passwordInput.type === "password") {
+        passwordInput.type = "text";
+        iconEye.querySelector("i")?.classList.replace("fa-eye-slash", "fa-eye");
+      } else {
+        passwordInput.type = "password";
+        iconEye.querySelector("i")?.classList.replace("fa-eye", "fa-eye-slash");
+      }
+    });
+  }
 
 }
